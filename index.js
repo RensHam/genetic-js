4b6dc6da27cb7c14aa3166b2ea885b885cbaff07
Rens Hamburger <renshamburger@gmail.com>
2021-02-24 18:24:48 +0100

    Fix exporting the correct class

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-export class Genetic;
-export function randomFromInterval;
+import Genetic from "./src/Genetic.js";
+import randomFromInterval from "./src/randomFromInterval.js"
+
+export {
+    Genetic,
+    randomFromInterval,
+};
